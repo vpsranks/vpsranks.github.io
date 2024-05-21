4a5034320a12e9efc27a03ea1db01e91b667b3ed
nomoomoo <nomoomoo@nomoomoo.nomoomoo>
2024-05-22 00:55:22 +0700

    vps

---
+++ static/shluqu/js/plugins.js
@@ -0,0 +1,34 @@
+/*
+	Theme Name: nll
+	Theme URI: nll
+	Description:nll
+	Version: 5.6.2
+	Author: nll
+	Author: nll
+
+*/
+(function() {
+    var method;
+    var noop = function() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+
+// Place any jQuery/helper plugins in here.
